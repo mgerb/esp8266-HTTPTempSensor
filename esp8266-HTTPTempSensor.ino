da08114ff47d7cea29b60fad13eccdce0987f5ae
mgerb42 <mgerb42@gmail.com>
2016-01-24 20:00:48 -0600

    updated with digitalSensor branch

--- esp8266-HTTPTempSensor.ino
+++ esp8266-HTTPTempSensor.ino
@@ -5,20 +5,17 @@
 #include <ESP8266mDNS.h>
 #include <EEPROM.h>
 #include <DHT.h>
-
 #include "page_index.h"
 
 #define DHTTYPE DHT11
-#define DHTPIN 5
+#define DHTPIN 4
 
 DHT dht(DHTPIN, DHTTYPE, 11);
 
 char ssid[32] = "";
 char password[32] = "";
 char sensorName[32] = "";
-
-//key used by web server to prevent unnecessary http requests
-const String server_password = "";
+char serverPassword[32] = "";
 
 const int httpPort = 80;
 const char* host = "www.mitchel.io";
@@ -39,6 +36,14 @@ WiFiClient client;
 /* Soft AP network parameters */
 IPAddress apIP(192, 168, 1, 1);
 IPAddress netMsk(255, 255, 255, 0);
+
+//function prototypes ----------
+void handleRoot();
+void handleConfig();
+void handleNotFound();
+void saveConfig();
+void loadCredentials();
+//------------------------------
 
 void setup() {
   // put your setup code here, to run once:
@@ -133,7 +138,7 @@ void loop() {
         break;
       }
 
-      delay(7000);
+      delay(5000);
       Serial.println("bad reading");
     }
     
@@ -146,7 +151,7 @@ void loop() {
     url += "&humidity=";
     url += humidity;
     url += "&key=";
-    url += server_password;
+    url += serverPassword;
     url += "&location=";
     url += sensorName;
 
@@ -164,6 +169,61 @@ void loop() {
     }
     
     delay(60000);
-    //ESP.deepSleep(120 * 1000000, WAKE_RF_DEFAULT);
   }
 }
+
+
+void handleRoot(){
+    server.send(200, "text/html", page_index);  
+}
+
+void handleConfig(){
+  
+  if (server.arg("password") != "" && server.arg("ssid") != "" && server.arg("name") != "" && server.arg("serverPassword") != ""){
+   
+    server.arg("ssid").toCharArray(ssid, sizeof(ssid) -1);
+    server.arg("password").toCharArray(password, sizeof(password) -1);
+    server.arg("name").toCharArray(sensorName, sizeof(sensorName) -1);
+    server.arg("serverPassword").toCharArray(serverPassword, sizeof(serverPassword) -1);
+    
+    saveConfig();
+
+    server.send(200, "text/html", "Config successful. Rebooting");
+
+    ESP.restart();
+  }
+
+  server.send(200, "text/html", "<script>window.onload = function() { window.location = \"/\"; }</script>");
+}
+
+void handleNotFound(){
+    server.send(404, "text/html", "404");
+}
+
+/** Store WLAN credentials to EEPROM */
+void saveConfig() {
+  EEPROM.begin(512);
+  EEPROM.put(0, ssid);
+  EEPROM.put(0+sizeof(ssid), password);
+  EEPROM.put(0+sizeof(ssid)+sizeof(password), sensorName);
+  EEPROM.put(0+sizeof(ssid)+sizeof(password)+sizeof(sensorName), serverPassword);
+  EEPROM.commit();
+  EEPROM.end();
+  
+}
+
+void loadCredentials() {
+  EEPROM.begin(512);
+  EEPROM.get(0, ssid);
+  EEPROM.get(0+sizeof(ssid), password);
+  EEPROM.get(0+sizeof(ssid)+sizeof(password), sensorName);
+  EEPROM.get(0+sizeof(ssid)+sizeof(password)+sizeof(sensorName), serverPassword);
+  EEPROM.end();
+  
+  Serial.println("Recovered credentials:");
+  Serial.println(ssid);
+  Serial.println(password);
+  Serial.println(sensorName);
+  Serial.println(serverPassword);
+}
+
--- page_index.h
+++ page_index.h
@@ -8,23 +8,26 @@ const char page_index[] PROGMEM = R"=====(
 </head>
 
 <body>
-	
+  
     <div class="container">
-    	
-		      <form action="config" method="post" class="form-signin">
-		        <h2 class="form-signin-heading center">Sensor Config</h2>
-		        <label for="inputSSID" class="sr-only">Access Point SSID</label>
-		        <input name="ssid" type="text" id="inputSSID" class="form-control" placeholder="SSID" required autofocus>
+      
+          <form action="config" method="post" class="form-signin">
+            <h2 class="form-signin-heading center">Sensor Config</h2>
+            <label for="inputSSID" class="sr-only">Access Point SSID</label>
+            <input name="ssid" type="text" id="inputSSID" class="form-control" placeholder="SSID" required autofocus>
             <br>
-		        <label for="inputPassword" class="sr-only">Password</label>
-		        <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-		        <br>
-		        <label for="inputName" class="sr-only">Sensor Name</label>
-		        <input name="name" type="text" id="inputName" class="form-control" placeholder="Sensor Name" required>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
             <br>
-		        <button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
-		      </form>
-	  	
+            <label for="inputName" class="sr-only">Sensor Name</label>
+            <input name="name" type="text" id="inputName" class="form-control" placeholder="Sensor Name" required>
+            <br>
+            <label for"inputServerPassword" class="sr-only">Server Password</label>
+            <input name="serverPassword" type="password" id="inputServerPassword" class="form-control" placeholder="Server Password" required>
+            <br>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+          </form>
+      
 
     </div> <!-- /container -->
 
